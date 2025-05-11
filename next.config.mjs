069d58af0cfd0c9c7cd06be3e21106158337e710
Michal Vesecký <michalvesecky@gmail.com>
2025-05-11 20:59:13 +0200

    Update next.config.mjs to use src/i18n/request.ts

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 import createNextIntlPlugin from 'next-intl/plugin';
 
-// Use the next-intl.config.js file
-const withNextIntl = createNextIntlPlugin();
+// Use the src/i18n/request.ts file
+const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
