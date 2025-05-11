738f214d4a7770566829d40302cffa7676e55915
Michal Vesecký <michalvesecky@gmail.com>
2025-05-11 21:02:11 +0200

    Add app/i18n/index.ts and update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 import createNextIntlPlugin from 'next-intl/plugin';
 
-// Use the src/i18n/request.ts file
-const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
+// Use the app/i18n/index.ts file
+const withNextIntl = createNextIntlPlugin('./app/i18n');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
